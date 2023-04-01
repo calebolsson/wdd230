2ab607f5dbfe345836aac2be6606638f3431de3b
calebolsson <caleb.l.olsson@gmail.com>
2023-04-01 16:57:01 -0500

    local storage system installed

--- finalproject/scripts/account.js
+++ finalproject/scripts/account.js
@@ -1,17 +1,21 @@
 let accountStorage = localStorage.getItem("account");
+function getFormValue(inputName) {
+  return document.getElementsByName(inputName).item(0).value;
+}
 
 function register(this_event) {
   this_event.preventDefault();
   if (accountStorage == null) {
-    let fname = document.getElementById("fname").value;
-    let lname = document.getElementById("lname").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
+    let fname = getFormValue("fname");
+    let lname = getFormValue("lname");
+    let email = getFormValue("email");
+    let phone = getFormValue("phone");
     console.log(fname, lname, email, phone);
     accountStorage = [fname, lname, email, phone];
     localStorage.setItem("account", [fname, lname, email, phone]);
     console.log(accountStorage);
     window.location.href = "index.html";
+    alert("Account creation sucessful");
   } else {
     console.log("Already logged in");
   }
@@ -21,6 +25,7 @@ const loginmask = document.getElementById("alreadyloggedin");
 function logout() {
   accountStorage = null;
   localStorage.removeItem("account");
+//   localStorage.clear();
 }
 
 if (localStorage.getItem("account") != null) loginmask.style.display = "flex";
--- finalproject/scripts/fresh.js
+++ finalproject/scripts/fresh.js
@@ -1,18 +1,44 @@
-let myDrinks = localStorage.getItem("myDrinks");
+let myDrinks;
 const drinkForm = document.getElementById("drink-designer");
 const mask = document.getElementById("freshchecklogin");
+function getFormValue(inputName) {
+  return document.getElementsByName(inputName).item(0).value;
+}
 
 if (localStorage.getItem("account") != null) mask.style.display = "none";
 
-function savedrink() {
-  if (myDrinks == null) {
+function saveDrink(this_event) {
+  this_event.preventDefault();
+  if (localStorage.getItem("myDrinks") == null) {
     myDrinks = [];
+  } else {
+    myDrinks = JSON.parse(localStorage["myDrinks"]);
   }
-  let drinkname = document.getElementsByName("drinkname").value;
-  let drinkbase = document.getElementsByName("drinkbase").value;
-  let ingredient1 = document.getElementsByName("ingredient1").value;
-  let ingredient2 = document.getElementsByName("ingredient2").value;
-  myDrinks.push([drinkname, drinkbase, ingredient1, ingredient2]);
+  let drinkname = getFormValue("drinkname");
+  let drinkbase = getFormValue("drinkbase");
+  let ingredient1 = getFormValue("ingredient1");
+  let ingredient2 = getFormValue("ingredient2");
+  let special = getFormValue("specialinstructions");
+  myDrinks.push([drinkname, drinkbase, ingredient1, ingredient2, special]);
+  console.log(myDrinks);
+  localStorage.setItem("myDrinks", JSON.stringify(myDrinks));
+  alert("Drink saved");
 }
 
-drinkForm.addEventListener("submit", savedrink());
+drinkForm.addEventListener("submit", (e) => saveDrink(e));
+
+const base = document.getElementById("selected");
+function selectBase(e) {
+  let thisColor = e.currentTarget.id;
+  base.textContent = thisColor;
+  if (base.classList.length == 0) base.classList.add(thisColor);
+  else base.classList.replace(base.classList.item(0), thisColor);
+  base.style.border = "2px solid black";
+  document.getElementById("insert-base").value = thisColor;
+}
+
+const fruitSelection = document.getElementById("selection");
+const fruitOptions = fruitSelection.children;
+for (i = 1; i < fruitOptions.length; i++) {
+  fruitOptions[i].addEventListener("click", selectBase);
+}
